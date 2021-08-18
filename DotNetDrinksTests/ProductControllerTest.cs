8a7799d2d4b0b1f907c4bb234ead1fb3d20c4b75
github-Tanyap1 <github-Tanya.Pachkalov@MyGeorgian.ca>
2021-08-18 13:30:15 -0400

    Write a Unit Test for the DeleteConfirmed

--- DotNetDrinksTests/ProductControllerTest.cs
+++ DotNetDrinksTests/ProductControllerTest.cs
@@ -6,12 +6,15 @@ using System.Text;
 using System.Threading.Tasks;
 using DotNetDrinks.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using DotNetDrinks.Models;
 
 namespace DotNetDrinksTests
 {
     [TestClass]
     public class ProductControllerTest
     {
+        public object Delete { get; private set; }
+
         //that passes a valid Id to the method and checks that the view that is returned is called “Delete”
         [TestMethod]
 
@@ -29,7 +32,24 @@ namespace DotNetDrinksTests
             var result = (ViewResult)ProductsController.Delete();
             Assert.IsNotNull("Delete, result.ViewName");
         }
+        //Write a Unit Test for the DeleteConfirmed Post method of the ProductsController that passes in one of the Product objects
+        //from the in-memory database and checks that this object is removed from the database successfully.
+
+        [TestMethod]
+        public void PostDeleteConfirmedProduct()
+        {
+            var result = Controller.Delete();
+            var viewResult = (ViewResult)result.Delete;
+            var model = (List<Product>)viewResult.Model;
+
+            CollectionAssert.AreEqual(Delete.Product.Id, model);
+        }
+
+
 
 
     }
+
+
+
 }
