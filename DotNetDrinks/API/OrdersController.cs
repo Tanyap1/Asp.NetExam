d899c51adc488e8a919a1633271a6bce4a8fb903
github-Tanyap1 <github-Tanya.Pachkalov@MyGeorgian.ca>
2021-08-18 12:15:22 -0400

    Swashbuckle and api folder

---
+++ DotNetDrinks/API/OrdersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DotNetDrinks.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotNetDrinks.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<OrdersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OrdersController>
+        [HttpPost]
+        public String Post([FromBody] string value)
+        {
+            return "Deleted Items";
+        }
+
+        // PUT api/<OrdersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrdersController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var response = new { success = false, massage = "OK" };
+            return new JsonResult(response);
+        }
+    }
+}
